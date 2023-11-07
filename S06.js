fd6e8566db023d716c4d8f587b172656b742dd34
amirizadi402 <147504792+amirizadi402@users.noreply.github.com>
2023-11-07 05:44:35 -0800

    Update S06.js
    routes

--- S06.js
+++ S06.js
@@ -4,10 +4,30 @@ server.listen(80);
 
 let headers = {"Content-Type" : "Text/Plain"};
 
+
+
 function requestHandler(request,response)
 {
+    let currentRoute = request.url.split('/');
+    let routes = 
+    {
+        sayHello : function (request,response)
+        {
+            response.write("salam"+ " " + request.url + " "+  request.method + '\n');
+            response.end();
+        },
+        "saySomeThing":function(request,response)
+        {
+            response.write( currentRoute[2] + '\n');
+            response.end();
+        },
+        "favicon.ico" : function(request,response)
+        {
+            response.write("fav" + '\n');
+            response.end();
+        }
+    }
 
-    response.writeHead(200,headers);
-    response.write("salam"+ " " + request.url + " "+  request.method);
-    response.end();
+    
+    routes[currentRoute[1]](request,response);
 }
